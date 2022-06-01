b11af7e9dd6d9277d80816a87085123796dede85
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-06-01 15:58:10 -0500

    Initial Commit

---
+++ MinimalVPN/MinimalVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalVPNApp.swift
+//  MinimalVPN
+//
+//  Created by Dr. Brandon Wiley on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MinimalVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
